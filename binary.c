2fca3417ceedab083c2a5c43368656d804e5dd5d
SafeCracker05 <142411735+SafeCracker05@users.noreply.github.com>
2023-12-31 14:58:06 +0200

    Update binary.c

--- binary.c
+++ binary.c
@@ -3,9 +3,11 @@
 
 void number_to_binary();
 void binary_to_number();
+void decimal_to_hexa_decimal();
 
 int n_b = 0;
 int b_n = 0;
+int d_hd =0;
 
 int main(void) {
     int choice;
@@ -14,6 +16,7 @@ int main(void) {
         printf("Converter binary\n");
         printf("1- Number to binary\n");
         printf("2- Binary to number\n");
+        printf("3-Decimal to Hexa decimal\n");
         printf("0- Exit\n");
         printf("Enter an option: ");
         scanf("%d", &choice);
@@ -28,6 +31,11 @@ int main(void) {
                 printf("Enter the binary: ");
                 scanf("%d", &b_n);
                 binary_to_number();
+                break;
+            case 3:
+                printf("Enter the decimal number: ");
+                scanf("%d", &d_hd);
+                decimal_to_hexa_decimal();
                 break;
             case 0:
                 printf("Exiting the program.\n");
@@ -80,4 +88,33 @@ void binary_to_number() {
         j++;
     }
     printf("%d\n", number);
+}
+
+void decimal_to_hexa_decimal(){
+     int quotient;
+    int remainder;
+    int i = 1;
+    char hexadecimalNumber[100];
+
+    quotient = d_hd;
+
+    while (quotient != 0) {
+        remainder = quotient % 16;
+
+        // Convertir le reste en caractère hexadécimal
+        if (remainder < 10)
+            hexadecimalNumber[i++] = remainder + '0';
+        else
+            hexadecimalNumber[i++] = remainder + 'A' - 10;
+
+        quotient = quotient / 16;
+    }
+
+    // Afficher le résultat en ordre inverse
+    printf("La représentation hexadécimale est : ");
+    for (int j = i - 1; j > 0; j--)
+        printf("%c", hexadecimalNumber[j]);
+
+    printf("\n");
+    
 }
